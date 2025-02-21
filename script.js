ed3c7000f84c4c96cacf41895882ac07eaab0fc8
JITENDRA KUMAR <jitendrakumar77800@gmail.com>
2025-02-21 07:28:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.getElementById("removeButton").addEventListener("click", function() {
+      const select = document.getElementById("colorSelect");
+      // Remove the selected option if one is chosen
+      if (select.selectedIndex !== -1) {
+        select.remove(select.selectedIndex);
+      }
+    });
